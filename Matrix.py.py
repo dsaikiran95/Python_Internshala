4318343d8d5afc67d331ab9aaffc87a487ce3096
D Sai Kiran <96724629+dsaikiran95@users.noreply.github.com>
2022-05-16 22:44:31 +0530

    Add files via upload

---
+++ Matrix.py.py
@@ -0,0 +1,32 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+
+
+# Program to add two matrices using nested loop
+
+X = [[6,7,3],
+    [8 ,1,6],
+    [7 ,8,3]]
+
+Y = [[2,8,1],
+    [6,7,3],
+    [4,5,9]]
+
+result = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+   # iterate through columns
+   for j in range(len(X[0])):
+       result[i][j] = X[i][j] + Y[i][j]
+
+for r in result:
+   print(r)
