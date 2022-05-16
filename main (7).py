75a5a051472fc0d0ecd4abf646a70b9ba98ff13d
D Sai Kiran <96724629+dsaikiran95@users.noreply.github.com>
2022-05-16 20:26:25 +0530

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,22 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+def NumberCheck(a):   
+    # Checking if the number is positive  
+    if a > 0:   
+        print("Number given by you is Positive")   
+    # Checking if the number is negative   
+    elif a < 0:   
+        print("Number given by you is Negative")   
+    # Else the number is zero  
+    else:   
+        print("Number given by you is zero")  
+# Taking number from user  
+a = float(input("Enter a number as input value: "))  
+# Printing result  
+NumberCheck(a)
