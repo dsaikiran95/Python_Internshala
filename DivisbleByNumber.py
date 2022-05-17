59d199d80f4fddb491e705ddf8496f6e7da2bce4
D Sai Kiran <96724629+dsaikiran95@users.noreply.github.com>
2022-05-17 11:52:09 +0530

    Add files via upload

---
+++ DivisbleByNumber.py
@@ -0,0 +1,17 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+#Python Program to Find Numbers Divisible by Another Number
+# Take a list of numbers
+my_list = [12, 65, 54, 39, 102, 339, 221,]
+
+# use anonymous function to filter
+result = list(filter(lambda x: (x % 13 == 0), my_list))
+
+# display the result
+print("Numbers divisible by 13 are",result)
